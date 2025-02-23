9e8b3cb5d06ad59e7c1219e72eb51be3aaeb6806
Darlington <59008938+fritzdultimate@users.noreply.github.com>
2025-02-23 20:57:00 +0100

    Update app.blade.php

--- resources/views/guest/layouts/app.blade.php
+++ resources/views/guest/layouts/app.blade.php
@@ -80,6 +80,18 @@
                 var x = document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(s, x); 
             })();
         </script>
+        <!-- Smartsupp Live Chat script -->
+<script type="text/javascript">
+var _smartsupp = _smartsupp || {};
+_smartsupp.key = '343fb181d60a04f51bfcb312d74eaaaa46dcc346';
+window.smartsupp||(function(d) {
+  var s,c,o=smartsupp=function(){ o._.push(arguments)};o._=[];
+  s=d.getElementsByTagName('script')[0];c=d.createElement('script');
+  c.type='text/javascript';c.charset='utf-8';c.async=true;
+  c.src='https://www.smartsuppchat.com/loader.js?';s.parentNode.insertBefore(c,s);
+})(document);
+</script>
+<noscript> Powered by <a href=“https://www.smartsupp.com” target=“_blank”>Smartsupp</a></noscript>
         @stack('bottom')
     </body>
 </html>
